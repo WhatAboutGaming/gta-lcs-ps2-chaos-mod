38721b8497a4622519d057f677e0a9dd5d37a3cc
Waggle <tsijesuswlg@gmail.com>
2025-09-15 04:30:00 +0000

    Fix incorrect regex for !github

--- gta_lcs_ps2_chaos_mod/server.js
+++ gta_lcs_ps2_chaos_mod/server.js
@@ -153,14 +153,14 @@ function onMessageHandler(target, tags, message, self) {
       finalUsername = username;
     }
     messageToWrite = message;
-    let githubPrefixCheck = /^[!\"#$%&'()*+,\-./:;%=%?@\[\\\]^_`{|}~¡¦¨«¬­¯°±»½⅔¾⅝⅞∅ⁿ№★†‡‹›¿‰℅æßçñ¹⅓¼⅛²⅜³⁴₱€¢£¥—–·„“”‚‘’•√π÷×¶∆′″§Π♣♠♥♪♦∞≠≈©®™✓‛‟❛❜❝❞❟❠❮❯⹂〝〞〟＂🙶🙷🙸󠀢⍻✅✔𐄂🗸‱]*\s*(github)+|(source(\s*code)*)+/ig.test(message);
+    let githubPrefixCheck = /^[!\"#$%&'()*+,\-./:;%=%?@\[\\\]^_`{|}~¡¦¨«¬­¯°±»½⅔¾⅝⅞∅ⁿ№★†‡‹›¿‰℅æßçñ¹⅓¼⅛²⅜³⁴₱€¢£¥—–·„“”‚‘’•√π÷×¶∆′″§Π♣♠♥♪♦∞≠≈©®™✓‛‟❛❜❝❞❟❠❮❯⹂〝〞〟＂🙶🙷🙸󠀢⍻✅✔𐄂🗸‱]+\s*((github)+|(source(\s*code)*)+)+/ig.test(message);
     if (githubPrefixCheck == true) {
       client.reply(target, "@" + finalUsername + " " + chatConfig.github_message + " " + chatConfig.github_repo, messageId);
     }
     let trustedUsersIndex = chatConfig.trusted_users.findIndex(element => element == userId);
     if (trustedUsersIndex >= 0) {
       // This is a trusted user
-      let toggleFrameLimiterPrefixCheck = /^[!\"#$%&'()*+,\-./:;%=%?@\[\\\]^_`{|}~¡¦¨«¬­¯°±»½⅔¾⅝⅞∅ⁿ№★†‡‹›¿‰℅æßçñ¹⅓¼⅛²⅜³⁴₱€¢£¥—–·„“”‚‘’•√π÷×¶∆′″§Π♣♠♥♪♦∞≠≈©®™✓‛‟❛❜❝❞❟❠❮❯⹂〝〞〟＂🙶🙷🙸󠀢⍻✅✔𐄂🗸‱]*\s*(toggle\s*frame\s*limiter)+/ig.test(message);
+      let toggleFrameLimiterPrefixCheck = /^[!\"#$%&'()*+,\-./:;%=%?@\[\\\]^_`{|}~¡¦¨«¬­¯°±»½⅔¾⅝⅞∅ⁿ№★†‡‹›¿‰℅æßçñ¹⅓¼⅛²⅜³⁴₱€¢£¥—–·„“”‚‘’•√π÷×¶∆′″§Π♣♠♥♪♦∞≠≈©®™✓‛‟❛❜❝❞❟❠❮❯⹂〝〞〟＂🙶🙷🙸󠀢⍻✅✔𐄂🗸‱]+\s*(toggle\s*frame\s*limiter)+/ig.test(message);
       if (toggleFrameLimiterPrefixCheck == true) {
         // Uhhh do the thing to change the framelimiter in the game's memory
         if (processObject == undefined) {
